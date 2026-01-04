a927ecfaf261328912cbbbed163382b4c609d6a0
Kevin-Kawai <kevin.fredricks@gmail.com>
2026-01-04 17:29:13 +0900

    remove comments

--- src/usecase/deleteSessionUseCase.ts
+++ src/usecase/deleteSessionUseCase.ts
@@ -3,16 +3,12 @@ import { SessionRepository } from "../repository/sessionRepository.ts";
 
 export class DeleteSessionUseCase {
   static async DeleteSession(projectName: string, sessionId: string) {
-    // Load the project
     const project = await ProjectRepository.find(projectName)
 
-    // Remove session from project's sessions array
     project.deleteSession(sessionId)
 
-    // Save updated project (without the deleted session reference)
     await ProjectRepository.save(project)
 
-    // Delete the session file from disk
     await SessionRepository.delete(sessionId)
   }
 }
