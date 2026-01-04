85839d885508454969e20a963cad135a483a8169
Kevin-Kawai <kevin.fredricks@gmail.com>
2026-01-04 14:24:11 +0900

    add find method to session repo and session factory

---
+++ src/factory/sessionFactory.ts
@@ -0,0 +1,25 @@
+import { Message } from "../domain/entities/Message.ts"
+import { Project } from "../domain/entities/Project.ts"
+import { Session } from "../domain/entities/Session.ts"
+
+export class SessionFactory {
+  sessionJson: any
+
+  constructor(sessionJson: any) {
+    this.sessionJson = sessionJson
+  }
+
+  generate() {
+    const project = new Project(this.sessionJson["project"]["path"], this.sessionJson["project"]["name"])
+    const session = new Session(
+      project,
+      this.sessionJson["claudeCodeSessionId"],
+      this.sessionJson["status"],
+      this.sessionJson["messages"]?.map((message: any) => {
+        return new Message({ messenger: message["messenger"], content: message["content"] })
+      }) ?? []
+    )
+
+    return session
+  }
+}
--- src/repository/sessionRepository.ts
+++ src/repository/sessionRepository.ts
@@ -1,6 +1,7 @@
 import { promises as fs } from "node:fs"
 import type { ISession } from "../domain/entities/Session.ts";
 import { normalizeCwd } from "../utils/normalizeCwd.ts";
+import { SessionFactory } from "../factory/sessionFactory.ts";
 
 const filePath = normalizeCwd("~/.lazystarforge/data")
 
@@ -9,6 +10,26 @@ export class SessionRepository {
     await fs.mkdir(filePath + "/sessions", { recursive: true })
     const sessionJson = JSON.stringify(await this.convertSessionToJson(session), null, 2)
     await fs.writeFile(filePath + `/sessions/${session.claudeCodeSessionId}.json`, sessionJson, "utf8")
+  }
+
+  static async find(sessionId: string) {
+    const rawSession = await fs.readFile(filePath + `/sessions/${sessionId}.json`, "utf8")
+    const parsedSession = JSON.parse(rawSession)
+    const sessionJson = await this.retrieveProject(parsedSession)
+
+    const sessionFactory = new SessionFactory(sessionJson)
+    const session = sessionFactory.generate()
+    return session
+  }
+
+  static async retrieveProject(session: any) {
+    const projectJson = await fs.readFile(filePath + `/projects/${session["project"]}.json`, "utf8")
+    const project = JSON.parse(projectJson)
+
+    return {
+      ...session,
+      project
+    }
   }
 
   static async convertSessionToJson(session: ISession) {
